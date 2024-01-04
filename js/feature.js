8819d151361b1150da73d4f83d2c04f78cc9b437
humbl3LilaS <sabishinekobebe@gmail.com>
2024-01-04 23:34:01 +0630

    desktop layout for feature section added, mobile nav bar's background blur effect added

--- js/feature.js
+++ js/feature.js
@@ -1,13 +1,17 @@
 const navList = document.getElementById("nav-list");
 const navBtn = document.getElementById("nav-btn");
+const nav = document.querySelector("nav");
+
 
 navBtn.addEventListener("click", () => {
   const isVisible = navList.getAttribute("data-visible");
   if (isVisible === "false") {
-    navList.setAttribute("data-visible", "true");
-    navBtn.setAttribute("aria-expanded", "true");
+	navList.setAttribute("data-visible", "true");
+	navBtn.setAttribute("aria-expanded", "true");
+	nav.setAttribute("data-backdrop", "true");
   } else {
-    navList.setAttribute("data-visible", "false");
-    navBtn.setAttribute("aria-expanded", "false");
+	navList.setAttribute("data-visible", "false");
+	navBtn.setAttribute("aria-expanded", "false");
+	nav.setAttribute("data-backdrop", "false");
   }
 });
